8e6a592b31cc82b891beb5523b8c477f1700b8cb
Andrew Fasano <fasano@mit.edu>
2019-11-30 17:05:29 -0500

    Min.py working with both concrete and symbolic data

--- min.py
+++ min.py
@@ -4,36 +4,55 @@ import keystone
 import capstone
 from ipdb import set_trace as d
 
-pc = 0x4000
-    #          00             ;  05
-    #          08                       ; 0b           ; 0e;       ; 10 ; 11
-shellcode  = b"mov eax, 0xcccc; movzx ebx, BYTE PTR [eax];"  + \
-             b"movzx  ecx,BYTE PTR [ebx];" + \
-             b"cmp ecx, 0x45; jne 0x4012; nop; jmp 0x4012;" + \
-             b"nop; " * 20
+START_PC = 0x4000
+EXPLORE_GOAL=START_PC+0xE # Address to find with angr
+sym_addr = 0xFFFF # Address to leave as symbolic
+shellcode  = [b"mov eax, 0xcccc",          # +0x00
+              b"mov ebx, DWORD PTR [eax]", # +0x05
+              b"mov ecx, DWORD PTR [ebx]", # +0x07
+              b"cmp ecx, 0x45",            # +0x09
+              b"jne 0x400F",               # +0x0C
+              b"nop",                      # +0x0E
+              b"jmp 0x400F",               # +0x0F # Loop forever.
+              b"nop",                      # When angr looks ahead,
+              b"nop",                      # make sure we always
+              b"nop",                      # have concrete instructions
+              ]
 
-# Shellcode will read memory at 0xCCCC, which we'll populate to be 0xFFFF JIT
-# then it will read memory at 0xFFFF which we'll leave as unconstrained
-# then there's a branch depending on the unconstrained value
-# and a bunch of nops so angr doesn't ever think the next instructions are unconstrained
+# Shellcode will read memory at 0xCCCC, which we'll populate to be 0xFFFF through a breakpoint (JIT)
+# then it will read memory at 0xFFFF which we'll leave as unconstrained data.
+# Then there's a branch depending on the unconstrained value
+# and an infinite loop at the end. Padded with nops so angr never reads ahead and sees symbolic code
 
 md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_32)
 ks = keystone.Ks(keystone.KS_ARCH_X86, keystone.KS_MODE_32)
 claripy_solver = claripy.Solver()
 
-insns, i_count = ks.asm(shellcode, pc)
+insns, i_count = ks.asm(b";".join(shellcode), START_PC)
+if i_count != len(shellcode): # Ensure our assembly compiles cleanly!
+    print("Failed to assemble shellcode")
+    for insn in shellcode:
+        success = False
+        try:
+            asm, success = ks.asm(insn) # PC doesn't matter
+        except:
+            pass
+        if not success:
+            print("Error compiling: {}".format(insn))
+    raise RuntimeError("Failed to compile input")
+
 asm = b"".join([bytes([x]) for x in insns])
-proj = angr.load_shellcode(asm, arch="i386", start_offset=pc, load_address=pc)
-start_state = proj.factory.blank_state(addr=pc)
+proj = angr.load_shellcode(asm, arch="i386", start_offset=START_PC, load_address=START_PC)
+start_state = proj.factory.blank_state(addr=START_PC)
+start_state.options.add(angr.options.SYMBOL_FILL_UNCONSTRAINED_MEMORY) # Silence unconstrained mem read warnings
 
 def jit_store(state):
     '''
-    Immediately before dereferencing 0xFFFF, set a value there
+    Immediately before dereferencing sym_addr, set a value there
     '''
     addr = state.inspect.mem_read_address
     addr_c = state.solver.eval(addr) # Could use claripy solver to check for multiple concrete values
     l = state.inspect.mem_read_length
-    l = 4 # XXX: What? Why do we see length of 1 when it's 4 at 0x4005
 
     # Could use claripy solver to check for multiple concrete values
     concrete_byte_val = bytes([0xff, 0xff, 0, 0]) #panda.virtual_memory_read(g_env, addr_c, l)
@@ -47,15 +66,15 @@ def jit_store(state):
     int_val = byte_int_val&mask
 
 
-    if addr_c == 0xFFFF:
-        print("Leave unconstrainted") # XXX: Do nothing - Use angr's default fallback
+    if addr_c == sym_addr:
+        print(f"JIT IGNORE: leave {l} bytes unconstrained at mem:0x{int_val}") # XXX: Do nothing - Use angr's default fallback
     else:
-        print(f"JIT STORE to address 0x{addr_c:x} value=0x{int_val:x} (len {l})")
+        print(f"JIT STORE: save value=0x{int_val:x} (len {l}) to address mem:0x{addr_c:x}")
         state.memory.store(addr_c, int_val, size=4) # Set the value
 
         # XXX: If we don't do this, the value isn't actually changed??? What??
         v = state.memory.load(addr_c)
-        assert(state.solver.eval(v) == int_val)
+        #assert(state.solver.eval(v) == int_val)
 
 def do_jit(state):
     '''
@@ -97,25 +116,24 @@ start_state.inspect.b('mem_read', condition=do_jit, action=jit_store, when=angr.
 
 simgr = proj.factory.simgr(start_state)
 
-simgr.explore(find=0x4010)
+simgr.explore(find=EXPLORE_GOAL)
 
 # After exploration we should have one item in `found` stash and one that's still active
 assert(len(simgr.active)), "Nothing in active stash"
 assert(len(simgr.found)),  "Nothing in found stash"
 
-sym_addr = 0xFFFF
-
-print(f"Evaluating mem:0x{sym_addr:x} in active state...")
+print(f"\nEvaluating mem:0x{sym_addr:x} in active state...")
 active = simgr.active[0]
 active_mem = active.memory.load(sym_addr, 1)
 active_conc = active.solver.eval(active_mem)
-print(f"0x{sym_addr:x} = 0x{active_conc:x} in active state...")
+print(f"mem:0x{sym_addr:x} = 0x{active_conc:x} in active state...")
 
-print(f"Evaluating mem:0x{sym_addr:x} in found state...")
+print(f"\nEvaluating mem:0x{sym_addr:x} in found state...")
 found = simgr.found[0]
 found_mem = found.memory.load(sym_addr, 1)
 found_conc = found.solver.eval(found_mem)
-print(f"0x{sym_addr:x} = 0x{found_conc:x} in found state...")
+print(f"mem:0x{sym_addr:x} = 0x{found_conc:x} in found state...")
 
 assert(active_conc != 0x45), "Active state incorrectly has soln value"
 assert(found_conc == 0x45), "Found state doesn't have soln value"
+print(f"Success, found state has solution value of 0x{found_conc:x}")
